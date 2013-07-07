7f3d73b6fa43698ee97820d61fd520d488f15d22
SIDS <aps.sids@gmail.com>
2013-07-08 02:02:16 +0530

    Added some views

---
+++ pilani_pirates/pirate_site/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, url
+from pirate_site import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<category>\w+)/$', views.category, name='category'),
+    url(r'^(?P<category>\w+)/(?P<id>\d+)/$', views.detail, name='detail'),
+)
--- pilani_pirates/pirate_site/views.py
+++ pilani_pirates/pirate_site/views.py
@@ -1,1 +1,30 @@
 # Create your views here.
+
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from pirate_site.models import *
+from django.db.models import get_app, get_models
+
+
+def index(request):
+    return HttpResponse("Search box + Welcome Page")
+
+
+def category(request, category):
+    for model in get_models(get_app('pirate_site')):
+        if category == model.__name__.lower():
+            files_list = model.objects.order_by('title').all()
+            output = '<br>'.join([e.title for e in files_list])
+            return HttpResponse(output)
+            break
+    return HttpResponse("No such category found.")
+
+
+def detail(request, category, id):
+    for model in get_models(get_app('pirate_site')):
+        if category == model.__name__.lower():
+            # files_list = model.objects.filter(id=id)
+            output = get_object_or_404(model, id=id)
+            return HttpResponse(output)
+            break
+    return HttpResponse("No such category found.")
