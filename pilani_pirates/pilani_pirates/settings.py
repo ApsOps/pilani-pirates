5e50e321894db2684c2d7388a0816d9ba5267acc
SIDS <aps.sids@gmail.com>
2013-07-22 18:49:18 +0530

    Added django south for migrations

--- pilani_pirates/pilani_pirates/settings.py
+++ pilani_pirates/pilani_pirates/settings.py
@@ -132,6 +132,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'pirate_site',
+    'south',
 )
 
 # A sample logging configuration. The only tangible logging
