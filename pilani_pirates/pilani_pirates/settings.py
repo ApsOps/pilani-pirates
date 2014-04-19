298e3e83c30c8ab1b942e598abe236ffe6763155
Amanpreet Singh <aps.sids@gmail.com>
2014-04-19 06:54:38 -0700

    uWSGI + nginx + solr setup working

--- pilani_pirates/pilani_pirates/settings.py
+++ pilani_pirates/pilani_pirates/settings.py
@@ -72,22 +72,22 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/var/www/static/pilani_pirates/'
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'collectedstatic')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/pilani_pirates/'
+STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
